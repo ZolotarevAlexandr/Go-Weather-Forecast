a8c1460e5ca21b6e96a5909632530fc5778bd67d
ZolotarevAlexandr <Zolotarev10032006@mail.ru>
2024-01-20 18:04:09 +0300

    Done: server side

--- main.go
+++ main.go
@@ -5,27 +5,36 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"time"
-	
+
+	"github.com/gin-gonic/gin"
 	"github.com/hectormalot/omgo"
 )
 
-type GeoResponce struct {
-	Results	[]LatLong	`json:"results"`
-}
 
 type LatLong struct {
 	Latitude	float64	`json:"latitude"`
 	Longitude	float64	`json:"longitude"`
 }
 
-type WeatherData struct {
-	DataTime 		[]time.Time
-	Temperatures	[]float64
-	Precipitations	[]float64
+type GeoResponce struct {
+	Results	[]LatLong	`json:"results"`
 }
+
+type HourWeatherData struct {
+	DateTime		time.Time
+	Temperature		float64
+	Precipitations	float64
+}
+
+type WeatherData struct {
+	City		string
+	Forecast	[]HourWeatherData
+}
+
 
 func getLangLong(city string) (*LatLong, error) {
 	client := http.Client{Timeout: 5 * time.Second}
@@ -60,15 +69,21 @@ func getLangLong(city string) (*LatLong, error) {
 	return &responce.Results[0], nil
 }
 
-func getWeather(latLong LatLong) (*WeatherData, error) {
+func getWeather(city string) (*WeatherData, error) {
 	client, err := omgo.NewClient()
 	if err != nil {
 		return nil, fmt.Errorf("error while creating omgo client: %w", err)
+	}
+
+	latLong, err := getLangLong(city)
+	if err != nil {
+		return nil, fmt.Errorf("error while searching city coordinates: %w", err)
 	}
 	loc, err := omgo.NewLocation(latLong.Latitude, latLong.Longitude)
 	if err != nil {
 		return nil, fmt.Errorf("error while searching omgo location: %w", err)
 	}
+
 	opts := omgo.Options{
 		HourlyMetrics: []string{"temperature_2m", "precipitation_probability"},
 	}
@@ -76,11 +91,49 @@ func getWeather(latLong LatLong) (*WeatherData, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error while getting omgo responce: %w", err)
 	}
-	return &WeatherData{resp.HourlyTimes,
-		resp.HourlyMetrics["temperature_2m"],
-		resp.HourlyMetrics["precipitation_probability"]}, nil
+
+	var forecast WeatherData
+	forecast.City = city
+	for i := 0; i < len(resp.HourlyTimes); i++ {
+		forecast.Forecast = append(forecast.Forecast, HourWeatherData{resp.HourlyTimes[i],
+			resp.HourlyMetrics["temperature_2m"][i],
+			resp.HourlyMetrics["precipitation_probability"][i]})
+	}
+	return &forecast, nil
+}
+
+func getHandler(ctx *gin.Context) {
+	city := ctx.Query("city")
+	weather, err := getWeather(city)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.HTML(http.StatusOK, "weather.html", *weather)
+}
+
+func getAPIHandler(ctx *gin.Context) {
+	city := ctx.Query("city")
+	weather, err := getWeather(city)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, *weather)
 }
 
 func main() {
-	// ...
+	ginServer := gin.Default()
+	ginServer.LoadHTMLGlob("views/*")
+
+	ginServer.GET("/weather", getHandler)
+	ginServer.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", nil)
+	})
+	ginServer.GET("/API/weather", getAPIHandler)
+
+	err := ginServer.Run("localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
